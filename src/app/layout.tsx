4b5ecc6a15aa21d5885841c7b038d4650d2df305
Arthur Tadevosyan <arthur.tadevosyan.dev@gmail.com>
2025-10-21 16:00:59 +0400

    new version with favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,9 +74,9 @@ export async function generateMetadata(): Promise<Metadata> {
     },
     icons: {
       icon: [
-        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-        { url: "/favicon.ico", sizes: "any" }
+        { url: "/favicon.png", sizes: "16x16", type: "image/png" },
+        { url: "/favicon.png", sizes: "32x32", type: "image/png" },
+        { url: "/favicon.png", sizes: "any" }
       ],
       apple: [
         { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
